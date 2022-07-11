db479e23d546054eb7b55715774f931d631de77e
SnowCharm <619022098@qq.com>
2022-07-11 21:40:29 +0800

    add category tree

--- mall-product/src/main/java/com/mall/product/service/impl/CategoryServiceImpl.java
+++ mall-product/src/main/java/com/mall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,13 @@
 package com.mall.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -16,6 +22,7 @@ import com.mall.product.service.CategoryService;
 @Service("categoryService")
 public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity> implements CategoryService {
 
+
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
         IPage<CategoryEntity> page = this.page(
@@ -24,6 +31,24 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+        return entities.stream()
+                .filter(o -> o.getParentCid() == 0)
+                .peek(menu -> menu.setChildren(getChildren(menu, entities)))
+                .sorted(Comparator.comparingInt(m -> (m.getSort() == null ? 0 : m.getSort())))
+                .toList();
+    }
+
+    private List<CategoryEntity> getChildren(CategoryEntity root, List<CategoryEntity> all) {
+        return all.stream()
+                .filter(entity -> Objects.equals(root.getCatId(), entity.getParentCid()))
+                .peek(entity -> entity.setChildren(getChildren(entity, all)))
+                .sorted(Comparator.comparingInt(m -> (m.getSort() == null ? 0 : m.getSort())))
+                .toList();
     }
 
 }
