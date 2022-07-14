727e546c486f28df8c94115bc2225afceff91255
SnowCharm <619022098@qq.com>
2022-07-14 21:36:49 +0800

    add third-party module

---
+++ mall-third-party/src/test/java/com/mall/mallthirdparty/MallThirdPartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mall.mallthirdparty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallThirdPartyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
