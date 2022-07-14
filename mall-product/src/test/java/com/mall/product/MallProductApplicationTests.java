cef0286c0f53bcfe3bd30d5631902a2c7e0c0421
SnowCharm <619022098@qq.com>
2022-07-14 20:44:38 +0800

    test file uploading in aliyun

--- mall-product/src/test/java/com/mall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/mall/product/MallProductApplicationTests.java
@@ -1,10 +1,18 @@
 package com.mall.product;
 
+import com.aliyun.oss.ClientException;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.OSSException;
 import com.mall.product.entity.BrandEntity;
 import com.mall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 @SpringBootTest
 class MallProductApplicationTests {
@@ -13,10 +21,53 @@ class MallProductApplicationTests {
     BrandService brandService;
 
     @Test
-    void test(){
+    void test1(){
         BrandEntity brandEntity = new BrandEntity();
         brandEntity.setName("Huawei");
         brandService.save(brandEntity);
+    }
+
+    @Test
+    void test2(){
+        // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
+        String endpoint = "oss-cn-shenzhen.aliyuncs.com";
+        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+        String accessKeyId = "LTAI5tA5FMZTiFuyEkxCxp4S";
+        String accessKeySecret = "CG7I4xD2VUwaGP1kzGaQQVrq8VtXVM";
+        // 填写Bucket名称，例如examplebucket。
+        String bucketName = "snowcharm";
+        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
+        String objectName = "优秀共青团员干部.png";
+        // 填写本地文件的完整路径，例如D:\\localpath\\examplefile.txt。
+        // 如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
+        String filePath= "D:\\1A\\重要资料\\优秀共青团干部.png";
+
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        try {
+            InputStream inputStream = new FileInputStream(filePath);
+            // 创建PutObject请求。
+            ossClient.putObject(bucketName, objectName, inputStream);
+        } catch (OSSException oe) {
+            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+                    + "but was rejected with an error response for some reason.");
+            System.out.println("Error Message:" + oe.getErrorMessage());
+            System.out.println("Error Code:" + oe.getErrorCode());
+            System.out.println("Request ID:" + oe.getRequestId());
+            System.out.println("Host ID:" + oe.getHostId());
+        } catch (ClientException ce) {
+            System.out.println("Caught an ClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with OSS, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message:" + ce.getMessage());
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (ossClient != null) {
+                ossClient.shutdown();
+            }
+        }
     }
 
 }
