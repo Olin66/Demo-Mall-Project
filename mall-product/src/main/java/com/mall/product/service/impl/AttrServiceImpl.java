1b2419723c3b37dd0500e06f7a2603181baffc7c
SnowCharm <619022098@qq.com>
2022-07-20 14:52:59 +0800

    update attribute query: category and group

--- mall-product/src/main/java/com/mall/product/service/impl/AttrServiceImpl.java
+++ mall-product/src/main/java/com/mall/product/service/impl/AttrServiceImpl.java
@@ -1,13 +1,19 @@
 package com.mall.product.service.impl;
 
 import com.mall.product.dao.AttrAttrgroupRelationDao;
+import com.mall.product.dao.AttrGroupDao;
+import com.mall.product.dao.CategoryDao;
 import com.mall.product.entity.AttrAttrgroupRelationEntity;
+import com.mall.product.entity.AttrGroupEntity;
+import com.mall.product.entity.CategoryEntity;
+import com.mall.product.vo.AttrRespVo;
 import com.mall.product.vo.AttrVo;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -27,6 +33,10 @@ public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements
 
     @Autowired
     AttrAttrgroupRelationDao relationDao;
+    @Autowired
+    AttrGroupDao attrGroupDao;
+    @Autowired
+    CategoryDao categoryDao;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -53,18 +63,37 @@ public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements
     @Override
     public PageUtils queryBaseAttrPage(Map<String, Object> params, Long catelogId) {
         QueryWrapper<AttrEntity> wrapper = new QueryWrapper<>();
-        if (catelogId != 0){
+        if (catelogId != 0) {
             wrapper.eq("catelog_id", catelogId);
         }
         String key = (String) params.get("key");
-        if(!StringUtils.isEmpty(key)){
+        if (!StringUtils.isEmpty(key)) {
             wrapper.and(wr -> wr.eq("attr_id", key).or().like("attr_name", key));
         }
         IPage<AttrEntity> page = this.page(
                 new Query<AttrEntity>().getPage(params),
                 wrapper
         );
-        return new PageUtils(page);
+        PageUtils pageUtils = new PageUtils(page);
+        List<AttrEntity> records = page.getRecords();
+        List<AttrRespVo> vos = records.stream().map(entity -> {
+            AttrRespVo vo = new AttrRespVo();
+            BeanUtils.copyProperties(entity, vo);
+            AttrAttrgroupRelationEntity relationEntity = relationDao
+                    .selectOne(new QueryWrapper<AttrAttrgroupRelationEntity>().eq("attr_id", entity.getAttrId()));
+            if (relationEntity != null) {
+                Long attrGroupId = relationEntity.getAttrGroupId();
+                AttrGroupEntity attrGroupEntity = attrGroupDao.selectById(attrGroupId);
+                vo.setGroupName(attrGroupEntity.getAttrGroupName());
+            }
+            CategoryEntity categoryEntity = categoryDao.selectById(entity.getCatelogId());
+            if (categoryEntity != null) {
+                vo.setCatelogName(categoryEntity.getName());
+            }
+            return vo;
+        }).toList();
+        pageUtils.setList(vos);
+        return pageUtils;
     }
 
 }
