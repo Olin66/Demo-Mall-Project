45d9a0e6f936c725121f21cb51925f00c4fb4e11
SnowCharm <619022098@qq.com>
2022-07-20 14:23:09 +0800

    add new attr by VO

---
+++ mall-product/src/main/java/com/mall/product/vo/AttrVo.java
@@ -0,0 +1,20 @@
+package com.mall.product.vo;
+
+
+import lombok.Data;
+
+@Data
+public class AttrVo {
+    private static final long serialVersionUID = 1L;
+    private Long attrId;
+    private String attrName;
+    private Integer searchType;
+    private Integer valueType;
+    private String icon;
+    private String valueSelect;
+    private Integer attrType;
+    private Long enable;
+    private Long catelogId;
+    private Integer showDesc;
+    private Long attrGroupId;
+}
