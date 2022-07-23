68ea0f1a9d990a72a13bbd757362fdc7ab6bf33b
SnowCharm <619022098@qq.com>
2022-07-23 11:32:38 +0800

    update querying sku

--- mall-frontend/src/views/modules/product/manager.vue
+++ mall-frontend/src/views/modules/product/manager.vue
@@ -100,8 +100,8 @@ export default {
       brandIdSub: null,
       dataForm: {
         key: "",
-        brandId: 0,
-        catelogId: 0,
+        brandId: "",
+        catelogId: "",
         price: {
           min: 0,
           max: 0
@@ -132,7 +132,7 @@ export default {
     //处理更多指令
     handleCommand(row, command) {
       console.log("~~~~~", row, command);
-      if ("stockSettings" == command) {
+      if ("stockSettings" === command) {
         this.$router.push({ path: "/ware-sku", query: { skuId: row.skuId } });
       }
     },
--- mall-product/src/main/java/com/mall/product/service/impl/SkuInfoServiceImpl.java
+++ mall-product/src/main/java/com/mall/product/service/impl/SkuInfoServiceImpl.java
@@ -1,7 +1,12 @@
 package com.mall.product.service.impl;
 
+import com.mall.product.entity.SpuInfoEntity;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -29,6 +34,45 @@ public class SkuInfoServiceImpl extends ServiceImpl<SkuInfoDao, SkuInfoEntity> i
     @Override
     public void saveSkuInfo(SkuInfoEntity skuInfoEntity) {
         this.baseMapper.insert(skuInfoEntity);
+    }
+
+    @Override
+    public PageUtils queryPageByCondition(Map<String, Object> params) {
+        QueryWrapper<SkuInfoEntity> wrapper = new QueryWrapper<>();
+        String key = (String) params.get("key");
+        if (!StringUtils.isEmpty(key)) {
+            wrapper.and(w -> w.eq("sku_id", key).or().like("sku_name", key));
+        }
+        String brandId = (String) params.get("brandId");
+        if (!StringUtils.isEmpty(brandId)) {
+            wrapper.eq("brand_id", brandId);
+        }
+        String catelogId = (String) params.get("catelogId");
+        if (!StringUtils.isEmpty(catelogId)) {
+            wrapper.eq("catalog_id", catelogId);
+        }
+        String min = (String) params.get("min");
+        String max = (String) params.get("max");
+        if (!StringUtils.isEmpty(min)){
+            wrapper.ge("price", min);
+        }
+        if (!StringUtils.isEmpty(max)){
+            try {
+                BigDecimal bigDecimal = new BigDecimal(max);
+                if(bigDecimal.compareTo(BigDecimal.ZERO)>0) {
+                    wrapper.le("price", max);
+                }
+            }catch (Exception e){
+                log.error("{}", e);
+            }
+            wrapper.le("price", max);
+        }
+
+        IPage<SkuInfoEntity> page = this.page(
+                new Query<SkuInfoEntity>().getPage(params),
+                wrapper
+        );
+        return new PageUtils(page);
     }
 
 }
