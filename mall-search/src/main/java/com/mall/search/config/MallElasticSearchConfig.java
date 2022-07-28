fd1eb4a4874611aaa611baea06ae4afbc2a806f7
SnowCharm <619022098@qq.com>
2022-07-28 18:34:06 +0800

    config elasticsearch

--- mall-search/src/main/java/com/mall/search/config/MallElasticSearchConfig.java
+++ mall-search/src/main/java/com/mall/search/config/MallElasticSearchConfig.java
@@ -1,8 +1,22 @@
 package com.mall.search.config;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MallElasticSearchConfig {
-
+    @Bean
+    public ElasticsearchClient esRestClient() {
+        RestClient restClient = RestClient.builder(
+                new HttpHost("192.168.56.10", 9200, "http")).build();
+        ElasticsearchTransport transport = new RestClientTransport(
+                restClient, new JacksonJsonpMapper());
+        return new ElasticsearchClient(transport);
+    }
 }
--- mall-search/src/test/java/com/mall/search/MallSearchApplicationTests.java
+++ mall-search/src/test/java/com/mall/search/MallSearchApplicationTests.java
@@ -1,13 +1,22 @@
 package com.mall.search;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class MallSearchApplicationTests {
 
+    @Autowired
+    private ElasticsearchClient client;
+
     @Test
-    void contextLoads() {
+    void test(){
+        System.out.println(client);
     }
 
 }
