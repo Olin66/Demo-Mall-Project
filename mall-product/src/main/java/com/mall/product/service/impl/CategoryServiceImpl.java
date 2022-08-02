e93f1f036faa87d10888a6b20683467c3ba66226
SnowCharm <619022098@qq.com>
2022-08-02 23:50:11 +0800

    add redis distributed lock

--- mall-product/src/main/java/com/mall/product/service/impl/CategoryServiceImpl.java
+++ mall-product/src/main/java/com/mall/product/service/impl/CategoryServiceImpl.java
@@ -17,6 +17,7 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -79,13 +80,39 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
         String catalogJSON = ops.get("catalogJSON");
         if (StringUtils.isEmpty(catalogJSON)) {
-            return getCatalogJsonFromDatabase();
+            return getCatalogJsonFromDatabaseWithRedisLock();
         }
         return JacksonUtils.toObj(catalogJSON, new TypeReference<>() {
         });
     }
 
-    public synchronized Map<String, List<CatalogSecondVo>> getCatalogJsonFromDatabase() {
+    public Map<String, List<CatalogSecondVo>> getCatalogJsonFromDatabaseWithRedisLock() {
+        String uuid = UUID.randomUUID().toString();
+        Boolean lock = stringRedisTemplate.opsForValue()
+                .setIfAbsent("lock", uuid, 300, TimeUnit.SECONDS);
+        while (true) {
+            if (Boolean.TRUE.equals(lock)) {
+                System.out.println("Locked!");
+                Map<String, List<CatalogSecondVo>> result;
+                try {
+                    result = getDataFromDatabase();
+                } finally {
+                    String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
+                    stringRedisTemplate.execute(new DefaultRedisScript<>(script), List.of("lock"), uuid);
+                }
+                return result;
+            }
+            lock = stringRedisTemplate.opsForValue()
+                    .setIfAbsent("lock", uuid, 300, TimeUnit.SECONDS);
+            System.out.println("Unlocked!");
+        }
+    }
+
+    public synchronized Map<String, List<CatalogSecondVo>> getCatalogJsonFromDatabaseWithLocalLock() {
+        return getDataFromDatabase();
+    }
+
+    private Map<String, List<CatalogSecondVo>> getDataFromDatabase() {
         ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
         String catalogJSON = ops.get("catalogJSON");
         if (!StringUtils.isEmpty(catalogJSON)) {
