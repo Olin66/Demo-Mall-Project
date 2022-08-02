2a67338e336df7dde5bfab712ac8a3c934044002
SnowCharm <619022098@qq.com>
2022-08-03 01:16:16 +0800

    test RReadWriteLock

--- mall-product/src/main/java/com/mall/product/web/IndexController.java
+++ mall-product/src/main/java/com/mall/product/web/IndexController.java
@@ -3,7 +3,11 @@ package com.mall.product.web;
 import com.mall.product.entity.CategoryEntity;
 import com.mall.product.service.CategoryService;
 import com.mall.product.vo.CatalogSecondVo;
+import org.redisson.api.RLock;
+import org.redisson.api.RReadWriteLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,14 +15,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 public class IndexController {
     @Autowired
     CategoryService categoryService;
 
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    RedissonClient redissonClient;
+
     @GetMapping({"/", "/index.html"})
-    public String indexPage(Model model){
+    public String indexPage(Model model) {
         List<CategoryEntity> categoryEntities = categoryService.getLevelOneCategories();
         model.addAttribute("categories", categoryEntities);
         return "index";
@@ -26,13 +37,48 @@ public class IndexController {
 
     @ResponseBody
     @GetMapping("/index/catalog.json")
-    public Map<String, List<CatalogSecondVo>> getCatelogJson(){
+    public Map<String, List<CatalogSecondVo>> getCatelogJson() {
         return categoryService.getCatalogJson();
     }
 
     @ResponseBody
     @GetMapping("/hello")
-    public String hello(){
+    public String hello() {
         return "hello";
+    }
+
+    @ResponseBody
+    @GetMapping("/write")
+    public String writeValue() {
+        RReadWriteLock lock = redissonClient.getReadWriteLock("rw-lock");
+        RLock rLock = lock.writeLock();
+        String s;
+        try {
+            rLock.lock();
+            s = UUID.randomUUID().toString();
+            stringRedisTemplate.opsForValue().set("value", s);
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            rLock.unlock();
+        }
+        return s;
+    }
+
+    @ResponseBody
+    @GetMapping("/read")
+    public String readValue() {
+        RReadWriteLock lock = redissonClient.getReadWriteLock("rw-lock");
+        RLock rLock = lock.readLock();
+        String s;
+        try {
+            rLock.lock();
+            s = stringRedisTemplate.opsForValue().get("value");
+        } finally {
+            rLock.unlock();
+        }
+
+        return s;
     }
 }
