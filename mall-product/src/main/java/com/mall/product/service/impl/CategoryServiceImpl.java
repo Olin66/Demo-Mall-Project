72191a7454726249135008511247f0f0de041b10
SnowCharm <619022098@qq.com>
2022-08-02 19:40:22 +0800

    add cache for CategoryServiceImpl.java

--- mall-product/src/main/java/com/mall/product/service/impl/CategoryServiceImpl.java
+++ mall-product/src/main/java/com/mall/product/service/impl/CategoryServiceImpl.java
@@ -1,8 +1,10 @@
 package com.mall.product.service.impl;
 
+import com.alibaba.nacos.common.utils.JacksonUtils;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.mall.common.utils.PageUtils;
 import com.mall.common.utils.Query;
 import com.mall.product.dao.CategoryDao;
@@ -11,7 +13,10 @@ import com.mall.product.service.CategoryBrandRelationService;
 import com.mall.product.service.CategoryService;
 import com.mall.product.vo.CatalogSecondVo;
 import com.mall.product.vo.CatalogThirdVo;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,6 +29,9 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
 
     @Autowired
     CategoryBrandRelationService categoryBrandRelationService;
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
 
 
     @Override
@@ -67,6 +75,19 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
 
     @Override
     public Map<String, List<CatalogSecondVo>> getCatalogJson() {
+        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+        String catalogJSON = ops.get("catalogJSON");
+        if (StringUtils.isEmpty(catalogJSON)) {
+            Map<String, List<CatalogSecondVo>> fromDatabase = getCatalogJsonFromDatabase();
+            String json = JacksonUtils.toJson(fromDatabase);
+            ops.set("catalogJSON", json);
+            return fromDatabase;
+        }
+        return JacksonUtils.toObj(catalogJSON, new TypeReference<>() {
+        });
+    }
+
+    public Map<String, List<CatalogSecondVo>> getCatalogJsonFromDatabase() {
         List<CategoryEntity> list = baseMapper.selectList(null);
         List<CategoryEntity> levelOne = getParentCid(list, 0L);
         return levelOne.stream().collect(Collectors
@@ -86,7 +107,7 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
                                     item.getCatId().toString(), item.getName());
                         }).toList();
                     }
-                    if (catalogSecondVos==null) return new ArrayList<>();
+                    if (catalogSecondVos == null) return new ArrayList<>();
                     else return catalogSecondVos;
                 }));
     }
