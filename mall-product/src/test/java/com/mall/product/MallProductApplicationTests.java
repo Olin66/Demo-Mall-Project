1dd1aba7c4fef90c38da5bbd89a8105e25b4a507
SnowCharm <619022098@qq.com>
2022-08-24 10:54:49 +0800

    handle aliyun security

--- mall-product/src/test/java/com/mall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/mall/product/MallProductApplicationTests.java
@@ -78,8 +78,6 @@ class MallProductApplicationTests {
 //        // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
 //        String endpoint = "oss-cn-shenzhen.aliyuncs.com";
 //        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-//        String accessKeyId = "LTAI5tA5FMZTiFuyEkxCxp4S";
-//        String accessKeySecret = "CG7I4xD2VUwaGP1kzGaQQVrq8VtXVM";
 //        // 填写Bucket名称，例如examplebucket。
 //        String bucketName = "snowcharm";
 //        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
