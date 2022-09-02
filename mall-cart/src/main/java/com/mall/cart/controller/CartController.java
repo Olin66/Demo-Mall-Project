db38b3c51e60b081b7400edcaa4ac9cd39512f6c
SnowCharm <619022098@qq.com>
2022-09-02 18:49:38 +0800

    config page jumping for shopping cart

--- mall-cart/src/main/java/com/mall/cart/controller/CartController.java
+++ mall-cart/src/main/java/com/mall/cart/controller/CartController.java
@@ -1,7 +1,5 @@
 package com.mall.cart.controller;
 
-import com.mall.cart.interceptor.CartInterceptor;
-import com.mall.common.to.UserInfoTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,7 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class CartController {
     @GetMapping("/cart.html")
     public String cartListPage() {
-        UserInfoTo user = CartInterceptor.threadLocal.get();
         return "cartList";
+    }
+
+    @GetMapping("/addToCart")
+    public String addToCart(){
+        return "success";
     }
 }
