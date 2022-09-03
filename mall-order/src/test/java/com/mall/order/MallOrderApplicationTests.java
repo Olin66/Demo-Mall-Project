12b09b291db97d04e0acd927eb34efd53884baa4
SnowCharm <619022098@qq.com>
2022-09-03 17:06:49 +0800

    config RabbitMQ for mall-order

--- mall-order/src/test/java/com/mall/order/MallOrderApplicationTests.java
+++ mall-order/src/test/java/com/mall/order/MallOrderApplicationTests.java
@@ -1,13 +1,22 @@
 package com.mall.order;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class MallOrderApplicationTests {
 
+    @Autowired
+    AmqpAdmin amqpAdmin;
+
     @Test
-    void contextLoads() {
+    void createExchange() {
+
     }
 
 }
