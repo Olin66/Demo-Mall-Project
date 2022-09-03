d93275d2079ab31fe0ed64b30539e137e55811ae
SnowCharm <619022098@qq.com>
2022-09-03 17:42:06 +0800

    test RabbitMQ

--- mall-order/src/test/java/com/mall/order/MallOrderApplicationTests.java
+++ mall-order/src/test/java/com/mall/order/MallOrderApplicationTests.java
@@ -1,11 +1,18 @@
 package com.mall.order;
 
+import com.mall.order.entity.OrderReturnReasonEntity;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -14,9 +21,32 @@ class MallOrderApplicationTests {
     @Autowired
     AmqpAdmin amqpAdmin;
 
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    @Test
+    void sendMessage() {
+        OrderReturnReasonEntity entity = new OrderReturnReasonEntity();
+        entity.setId(1L);
+        entity.setName("awd");
+        entity.setCreateTime(new Date());
+        rabbitTemplate.convertAndSend("java-exchange", "hello.java", entity);
+    }
+
     @Test
     void createExchange() {
+        amqpAdmin.declareExchange(new DirectExchange("java-exchange", true, false));
+    }
 
+    @Test
+    void createQueue() {
+        amqpAdmin.declareQueue(new Queue("java-queue", true, false, false));
+    }
+
+    @Test
+    void createBinding() {
+        amqpAdmin.declareBinding(new Binding("java-queue", Binding.DestinationType.QUEUE,
+                "java-exchange", "hello.java", null));
     }
 
 }
