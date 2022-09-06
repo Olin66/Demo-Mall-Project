7a920ab98c5b90d9e8f210b5738db83c0300d9fb
SnowCharm <619022098@qq.com>
2022-09-06 10:03:39 +0800

    config JAutoProxy

--- mall-order/src/main/java/com/mall/order/MallOrderApplication.java
+++ mall-order/src/main/java/com/mall/order/MallOrderApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
@@ -12,6 +13,7 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @EnableRabbit
 @EnableRedisHttpSession
 @EnableFeignClients
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class MallOrderApplication {
 
     public static void main(String[] args) {
