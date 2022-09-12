be634d1522f469af1142b63699730ae886c7d5e0
SnowCharm <619022098@qq.com>
2022-09-12 22:46:46 +0800

    add mall-seckill

--- mall-coupon/src/main/java/com/mall/coupon/service/impl/SeckillSkuRelationServiceImpl.java
+++ mall-coupon/src/main/java/com/mall/coupon/service/impl/SeckillSkuRelationServiceImpl.java
@@ -1,16 +1,17 @@
 package com.mall.coupon.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
+import com.alibaba.cloud.commons.lang.StringUtils;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.mall.common.utils.PageUtils;
 import com.mall.common.utils.Query;
-
 import com.mall.coupon.dao.SeckillSkuRelationDao;
 import com.mall.coupon.entity.SeckillSkuRelationEntity;
 import com.mall.coupon.service.SeckillSkuRelationService;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("seckillSkuRelationService")
@@ -18,9 +19,14 @@ public class SeckillSkuRelationServiceImpl extends ServiceImpl<SeckillSkuRelatio
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<SeckillSkuRelationEntity> wrapper = new QueryWrapper<>();
+        String promotionSessionId = (String) params.get("promotionSessionId");
+        if (!StringUtils.isEmpty(promotionSessionId)) {
+            wrapper.eq("promotion_session_id", promotionSessionId);
+        }
         IPage<SeckillSkuRelationEntity> page = this.page(
                 new Query<SeckillSkuRelationEntity>().getPage(params),
-                new QueryWrapper<SeckillSkuRelationEntity>()
+                wrapper
         );
 
         return new PageUtils(page);
