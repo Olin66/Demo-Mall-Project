e75628227dbc6802ba90624ee8df2dff28b0a6ec
SnowCharm <619022098@qq.com>
2022-09-25 17:41:42 +0800

    config return type for Sentinel failing in the frontend

--- mall-common/src/main/java/com/mall/common/exception/ExceptionCodeEnum.java
+++ mall-common/src/main/java/com/mall/common/exception/ExceptionCodeEnum.java
@@ -4,6 +4,7 @@ public enum ExceptionCodeEnum {
     UNKNOWN_EXCEPTION(10000, "系统未知异常"),
     VALID_EXCEPTION(10001, "参数格式校验失败"),
     SMS_CODE_EXCEPTION(10002, "短信验证码频率过高"),
+    TOO_MANY_REQUEST_EXCEPTION(10003, "发送请求过多"),
     PRODUCT_UP_EXCEPTION(11000, "商品上架异常"),
     USERNAME_EXISTED_EXCEPTION(15001, "用户名已经存在"),
     PHONE_EXISTED_EXCEPTION(15002, "手机号已经存在"),
